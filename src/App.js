80dd53fd9084d459fc8e4319025706d47ef4fa2a
heyDeriPujokisworo <heyderi.pujokisworo88@gmail.com>
2023-04-03 01:45:13 +0700

    Tugas reactJS portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./component/Home";
+import NavBar from "./component/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
